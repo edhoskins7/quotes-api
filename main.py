7b180abe941228847845d2a721142af9a4f1905a
NuclearWinterUK <elenkis@gmail.com>
2022-05-18 16:25:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func, select
+
+API_KEY = "12345"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quotes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Quote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, unique=True, nullable=False)
+    quote = db.Column(db.String(500), unique=True, nullable=False)
+db.create_all()
+
+
+@app.route("/add-quote", methods=["GET", "POST"])
+def add_quote():
+    if request.args.get("api_key") == API_KEY:
+        all_quotes = db.session.query(Quote).all()
+        new_quote = Quote(number=len(all_quotes) + 1, quote=request.args.get("data"))
+        db.session.add(new_quote)
+        db.session.commit()
+        return "Quote added."
+    else:
+        return jsonify(error={"Not Allowed": "Please provide a valid api key."})
+
+
+@app.route("/del-quote", methods=["GET", "DELETE"])
+def del_quote():
+    if request.args.get("api_key") == API_KEY:
+        quote_to_delete = Quote.query.filter_by(number=f'{request.args.get("data")}').first()
+        if quote_to_delete:
+            db.session.delete(quote_to_delete)
+            all_quotes = Quote.query.order_by(Quote.number).all()
+            for i in range(len(all_quotes)):
+                all_quotes[i].number = i + 1
+            db.session.commit()
+            return "Quote deleted."
+        else:
+            return "Please use a valid quote number."
+    else:
+        return jsonify(error={"Not Allowed": "Please provide a valid api key."})
+
+
+@app.route("/quote", methods=["GET"])
+def get_quote():
+    if request.args.get('data') == "list":
+        return "test"
+    elif request.args.get('data') == "":
+        quote = Quote.query.order_by(func.random()).first()
+        return quote.quote
+    elif request.args.get('data').isnumeric():
+        lookup = Quote.query.filter(Quote.number.ilike(f"{request.args.get('data')}")).first()
+        if not lookup:
+            not_found = "Sorry, quote not found."
+            return not_found
+        else:
+            return lookup.quote
+    else:
+        lookup = Quote.query.filter(Quote.quote.regexp_match(rf"\b(?i){request.args.get('data')}")).first()
+        if not lookup:
+            not_found = "Sorry, quote not found."
+            return not_found
+        else:
+            quote = Quote.query.filter(Quote.quote.regexp_match(rf"\b(?i){request.args.get('data')}")).order_by(func.random()).first()
+            return quote.quote
+
+
+@app.route("/quote/list")
+def quote_list():
+    all_quotes = db.session.query(Quote).all()
+    return render_template('list.html', all_quotes=all_quotes)
+
+
+if __name__ == '__flask__':
+    app.run(debug=True)
